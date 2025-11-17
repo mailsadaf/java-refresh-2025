80aa61c5c893f3fe354a888436a0d1bd6495a734
Sadaf <mailsadaf@gmail.com>
2025-11-17 22:08:33 +0100

    Sorted Even Squares

---
+++ src/learn/java/lambda_streaming/SortedEvenSquares.java
@@ -0,0 +1,13 @@
+package learn.java.lambda_streaming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortedEvenSquares {
+    public static void main(String[] args) {
+        List<Integer> input = Arrays.asList(3,10,1,20,5,6);
+        List<Integer> evenInputs = input.stream().filter(n->n%2==0).collect(Collectors.toList());
+        evenInputs.forEach(evenInt-> System.out.println(evenInt*evenInt));
+    }
+}
